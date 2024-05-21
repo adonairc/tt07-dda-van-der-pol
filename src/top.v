2f61a5d484e31c512f17c835310aec666c549b40
Adonai <adonairc@gmail.com>
2024-05-20 20:06:40 -0700

    Top module renamed

--- src/top.v
+++ src/top.v
@@ -7,7 +7,7 @@
 `include "dda.v"
 `include "posit.v"
 
-module tt_um_example (
+module tt_um_adonairc_dda (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
